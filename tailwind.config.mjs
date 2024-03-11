9af5f84c7e226be8e3966338ebfa64ccb35a644b
Braxton Fair <hello@taxborn.com>
2024-03-11 00:14:34 -0500

    update fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: ['JetBrains Mono Variable', ...defaultTheme.fontFamily.mono],
+				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 }
